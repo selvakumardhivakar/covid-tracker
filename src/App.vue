91a5568178fa73209854085bf4ed392484a3e0cf
selvakumardhivakar <selvakumardhivakar@gmail.com>
2020-03-20 02:34:58 +0530

    In:hammer it commit �

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="page">
+    <h3>Corono Updates 🦠</h3>
+    <div v-if="totalCases!==''" class="container">
+      <h4>Total No of cases:{{totalCases}}</h4>
+      <h4>Total No of death:{{death}}</h4>
+      <h4>Total No of recovered:{{recovered}}</h4>
+    </div>
+    <p v-else>Loading...</p>
+    <div class="container country-data">
+      <form @submit.prevent="fetchCountryData" class="container country-form">
+        <input v-model="countryName" type="text">
+        <button type="submit">Submit</button>
+      </form>
+      <hr>
+      <div v-if="countryData!==''" class="container">
+        <h4>Country Name: {{countryData.country}}</h4>
+        <h4>Total Cases: {{countryData.cases}}</h4>
+        <h4>Total Deaths: {{countryData.deaths}}</h4>
+        <h4>Active Cases: {{countryData.active}}</h4>
+        <h4>Recovered Cases: {{countryData.recovered}}</h4>
+        <h4>Critical Cases: {{countryData.critical}}</h4>
+        <h3>Today</h3>
+        <h4>Today Cases: {{countryData.todayCases}}</h4>
+        <h4>Today Deaths: {{countryData.todayDeaths}}</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import service from "./services";
+export default {
+  data() {
+    return {
+      countryData: "",
+      countryName: "",
+      totalCases: "",
+      death: "",
+      recovered: ""
+    };
+  },
+  created() {
+    service.getData().then(res => {
+      this.totalCases = res.data.cases;
+      this.death = res.data.deaths;
+      this.recovered = res.data.recovered;
+    });
+  },
+  methods: {
+    fetchCountryData() {
+      if (this.countryName.trim() !== "") {
+        console.log(this.countryName);
+        service
+          .getCountryData(this.countryName)
+          .then(res => res.data)
+          .then(data => {
+            this.countryData = data;
+            console.log(this.countryData);
+          });
+      }
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  line-height: 1.5;
+}
+.container {
+  margin: 2rem auto;
+  text-align: center;
+}
+.country-data {
+  margin: 1rem auto;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: baseline;
+}
+
+.country-form input {
+  padding: 0.5rem;
+  margin: 1rem;
+  font-size: 1.2rem;
+}
+.country-form button {
+  padding: 0.5rem 1.5rem;
+  margin: 0.5rem;
+  border: none;
+  font-size: 1.2rem;
+  background-color: grey;
+}
+.country-form {
+  border-bottom: 1px solid black;
+}
+.page {
+  background-color: rgba(0, 0, 0, 0.1);
+  text-align: center;
+  font-size: 2.5rem;
+  height: 100%;
+  min-height: 100vh;
+  color: green;
+}
+
+.page h3 {
+  text-decoration: underline;
+}
+</style>
---
+++ src/services.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `https://corona.lmao.ninja/`,
+  withCredentials: false, // This is the default
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
+export default {
+  getData() {
+    return apiClient.get("/all");
+  },
+  getCountryData(name) {
+    return apiClient.get("/countries/" + name);
+  }
+};
